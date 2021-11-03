a68452b39f8b611a90ca0306412608fd91932055
Faith Mofokeng <faithmofokeng5@gmail.com>
2021-11-03 23:19:22 +0200

    removed error catches

--- src/express.js
+++ src/express.js
@@ -7,49 +7,38 @@ const pool = require("./pool")
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
 
-pool.connect(function(err) {
-  if (err) console.log(err + " Ooops");
-  else console.log("Pool connected!");
+pool.connect(() => {
+  console.log("Pool connected!");
 });
 
-app.use(express.static('public'));
 app.listen(2000, () => console.log(`http://localhost:${2000}/new_visit`));
 
 const createTable = () => {
-  pool.query(`CREATE TABLE Visitors ( id SERIAL PRIMARY KEY, name VARCHAR(50), age INT, date  DATE, time TIME, assistor VARCHAR(50), comments VARCHAR(100))`,
-    (req, res) => {
-      console.log(req, res)
-    }  
-  )
+  pool.query(`CREATE TABLE Visitors ( id SERIAL PRIMARY KEY, name VARCHAR(50), age INT, date  DATE, time TIME, assistor VARCHAR(50), comments VARCHAR(100))`)
 }
 
 const addNewVisitors = (dataObj) => {
     pool.query(
       `INSERT INTO Visitors (name, age, date, time, assistor, comments) 
       VALUES ($1, $2, $3, $4, $5, $6)`,
-      [dataObj.name,dataObj.age,dataObj.date,dataObj.time,dataObj.assistor,dataObj.comments],
-      (err) => {
-        if (err) {throw new Error (err)}
-        console.log(JSON.stringify(dataObj));
-      });
+      [dataObj.name,dataObj.age,dataObj.date,dataObj.time,dataObj.assistor,dataObj.comments])
       return JSON.stringify(dataObj)
 
 };
 
-let add = addNewVisitors({'name': 'Faith', 
+const add = addNewVisitors({'name': 'Faith', 
 'age': 45, 'date': '2010-09-12', 
 'time': '19:00', 'assistor': 'Kim', 
 'comments': 'Nothing'})
 
 app.set('views', path.join(__dirname, '../views'))
 app.set('view engine', 'pug')
-app.get('/new_visit', (req, res) => {
+app.get('/new_visit', (_,res) => {
   res.render('views', {title: 'Your data: ', data: `${add}`})
 });
 const listAllVisitors = () => {
-  pool.query( "SELECT DISTINCT ID, Name FROM Visitors", (error, respond) => {
-      console.log(error, respond);
-  });
+  pool.query( "SELECT DISTINCT ID, Name FROM Visitors");
 }
 
