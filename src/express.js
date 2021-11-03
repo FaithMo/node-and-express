63fd053e4e8fac25565f044f34d66d057d9a1c24
Faith Mofokeng <faithmofokeng5@gmail.com>
2021-11-03 23:08:19 +0200

    saved data redirects to the thank you pug

--- src/express.js
+++ src/express.js
@@ -1,7 +1,6 @@
 //======================STATIC=FILE=SERVER================================
 
 const express = require('express');
-const port = 2000;
 const bodyParser = require('body-parser');
 const path = require('path')
 const pool = require("./pool")
@@ -11,43 +10,42 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 pool.connect(function(err) {
   if (err) console.log(err + " Ooops");
-  else console.log("Connected!");
+  else console.log("Pool connected!");
 });
 
 app.use(express.static('public'));
+app.listen(2000, () => console.log(`http://localhost:${2000}/new_visit`));
 
-app.listen(port, () => console.log(`http://localhost:${port}/new_visit`));
-
-// app.post('/database-approval', (req, res) => {
-//   addNewVisitors(req.body);
-//   res.send(`Thanks for the info! The following was saved to the database: ${JSON.stringify(req.body)}`);
-// });
-
-
-//============================FUNCTIONS=====================================c
+const createTable = () => {
+  pool.query(`CREATE TABLE Visitors ( id SERIAL PRIMARY KEY, name VARCHAR(50), age INT, date  DATE, time TIME, assistor VARCHAR(50), comments VARCHAR(100))`,
+    (req, res) => {
+      console.log(req, res)
+    }  
+  )
+}
 
 const addNewVisitors = (dataObj) => {
     pool.query(
-      `INSERT INTO Visitors (Name, Age, Date, Time, Assistor, Comments) 
+      `INSERT INTO Visitors (name, age, date, time, assistor, comments) 
       VALUES ($1, $2, $3, $4, $5, $6)`,
       [dataObj.name,dataObj.age,dataObj.date,dataObj.time,dataObj.assistor,dataObj.comments],
-      (error) => {
-        if (error) {throw new Error (error)}
+      (err) => {
+        if (err) {throw new Error (err)}
         console.log(JSON.stringify(dataObj));
-        return JSON.stringify(dataObj)
       });
+      return JSON.stringify(dataObj)
+
 };
 
-let add = addNewVisitors({'Name': 'Faith', 
-'Age': 23, 'Date': '2010-09-12', 
-'Time': '19:00', 'Assistor': 'Kim', 
-'Comments': 'Nothing'})
-console.log(add)
+let add = addNewVisitors({'name': 'Faith', 
+'age': 45, 'date': '2010-09-12', 
+'time': '19:00', 'assistor': 'Kim', 
+'comments': 'Nothing'})
 
 app.set('views', path.join(__dirname, '../views'))
 app.set('view engine', 'pug')
 app.get('/new_visit', (req, res) => {
-  res.render('views', `Your data: ${add}`)
+  res.render('views', {title: 'Your data: ', data: `${add}`})
 });
 const listAllVisitors = () => {
   pool.query( "SELECT DISTINCT ID, Name FROM Visitors", (error, respond) => {
