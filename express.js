189dbc83da761402d5f6e0f461b23d16ac580c23
FaithMo <faithmofokeng5@gmail.com>
2021-11-03 16:18:28 +0200

    added pug

--- express.js
+++ express.js
@@ -1,10 +1,11 @@
 //======================STATIC=FILE=SERVER================================
 
-const express = require ('express');
+const express = require('express');
 const app = express();
 const port = 2000;
 const bodyParser = require('body-parser');
 const Http = require('http');
+// const pool = require("./pool")
 
 app.get('/', (req, res) => res.send('Static file expressJS'));
 app.use(bodyParser.urlencoded({extended: false}));
@@ -14,7 +15,6 @@ app.use(express.static('public'));
 app.listen(port, () => console.log(`http://localhost:${port}/new_visit`));
 
 //=======================DATABASE==SERVER================================
-
 const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "user3",
@@ -22,9 +22,10 @@ const pool = new Pool({
     database: "db",
     password: "pass3",
     port: 5432
-  });
+  })
+module.exports = pool
   
-var dbConnection = pool.connect(function(err) {
+let dbConnection = pool.connect(function(err) {
     if (err) console.log(err + " Ooops");
     else console.log("Adminer 4.7.5 server Connected!");
   });
@@ -32,7 +33,6 @@ var dbConnection = pool.connect(function(err) {
 app.post('/database-approval', (req, res) => {
   addNewVisitors(req.body);
   res.send(`Thanks for the info! The following was saved to the database: ${JSON.stringify(req.body)}`);
-  //res.send(JSON.stringify(req.body))
 });
 
 
@@ -43,7 +43,7 @@ const addNewVisitors = (dataObj) => {
       `INSERT INTO Visitors (Name, Age, Date, Time, Assistor, Comments) 
       VALUES ($1, $2, $3, $4, $5, $6)`,
       [dataObj.name,dataObj.age,dataObj.date,dataObj.time,dataObj.assistor,dataObj.comments],
-      (error, results) => {
+      (error) => {
         if (error) {throw new Error (error)}
         console.log(JSON.stringify(dataObj));
       });
