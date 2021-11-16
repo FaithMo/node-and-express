4cd8672588e084059fcfc80f23580e1cb639351a
FaithMo <faithmofokeng5@gmail.com>
2021-11-16 13:51:44 +0200

    removed unused code & added default connection strings

--- src/pool.js
+++ src/pool.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: process.env.user,
-  host: process.env.host,
-  database: process.env.database,
-  password: process.env.password,
-  port: process.env.port,
+  user: process.env.user || "user3",
+  host: process.env.host || "pass3",
+  database: process.env.database || "db",
+  password: process.env.password || "pass3",
+  port: process.env.port || "1000",
 });
 
 module.exports = {pool}
