88a73267e3c1e6b713bdd108a048773c8e6c9eb1
FaithMo <faithmofokeng5@gmail.com>
2021-11-17 08:28:54 +0200

    fixed config

--- src/pool.js
+++ src/pool.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const Pool = require("pg").Pool;
 const pool = new Pool({
   user: process.env.user || "user3",
-  host: process.env.host || "pass3",
+  host: process.env.host || "localhost",
   database: process.env.database || "db",
-  password: process.env.password || "pass3",
+  password: process.env.password,
   port: process.env.port || "1000",
 });
 
